71f7fb408fb8041a7306b4c54549969cbcb23d30
Bruno Casali <brunoocasali@gmail.com>
2016-06-08 14:22:05 -0300

    Updated base controllers and other files about views:
    - Added grayscale theme
    - Devise configured
    - Migrations fixed

--- app/enumerations/user_role.rb
+++ app/enumerations/user_role.rb
@@ -1,3 +1,3 @@
 class UserRole < EnumerateIt::Base
-  associate_values admin: 1, client: 2, print_requestor: 3
+  associate_values admin: 1, client: 2, print_requestor: 3, root: 4
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'welcome/index'
+
   devise_for :users
+
+  authenticated :user do
+    root 'welcome#index', as: :authenticated_root
+  end
+
+  # get :find_cities, on: :collection
+
+  root 'home#index' # to: redirect('/users/sign_in')
 end
