a3fc081761f3a7041eb70dbf609b6e5912292a88
Bruno Casali <brunoocasali@gmail.com>
2016-06-08 19:03:38 -0300

    created base for dashboards in projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608201435) do
+ActiveRecord::Schema.define(version: 20160608214254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20160608201435) do
     t.integer  "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   add_index "projects", ["code"], name: "index_projects_on_code", unique: true, using: :btree
