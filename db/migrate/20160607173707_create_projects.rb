ecb8188b4cd33e4f8f67b501a9fe236950cd69f2
Bruno Casali <brunoocasali@gmail.com>
2016-06-08 10:25:09 -0300

    fixed error about onwers and users

--- db/migrate/20160607173707_create_projects.rb
+++ db/migrate/20160607173707_create_projects.rb
@@ -3,11 +3,12 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.integer :status, index: true
       t.string :code
-      t.references :owner, index: true, foreign_key: true
+      t.references :owner, index: true
 
       t.timestamps null: false
     end
 
     add_index :projects, :code, unique: true
+    add_foreign_key :projects, :users, column: :owner_id
   end
 end
