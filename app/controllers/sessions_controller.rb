8b5b9535f688a4185c4f5510028f2e0a10946843
Bruno Casali <brunoocasali@gmail.com>
2016-06-09 16:43:49 -0300

    generated blank scaffold controller for sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,48 @@
+class SessionsController < ApplicationController
+  before_action :set_session, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @sessions = Session.all
+  end
+
+  def show; end
+
+  def new
+    @session = Session.new
+  end
+
+  def edit; end
+
+  def create
+    @session = Session.new(session_params)
+
+    if @session.save
+      redirect_to @session, notice: 'Session was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @session.update(session_params)
+      redirect_to @session, notice: 'Session was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @session.destroy
+    redirect_to sessions_url, notice: 'Session was successfully destroyed.'
+  end
+
+  private
+
+  def set_session
+    @session = Session.find(params[:id])
+  end
+
+  def session_params
+    params[:session]
+  end
+end
