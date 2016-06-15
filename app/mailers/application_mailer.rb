60e9f9757f9d54c50531627c9e19e94b61a35e24
Bruno Casali <brunoocasali@gmail.com>
2016-06-15 17:42:24 -0300

    trying to fix layout

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'T. wacelkoski Fotografias <no-reply@wacelkoski.com>'
+  default from: 'T. Wacelkoski Fotografias <no-reply@wacelkoski.com>'
 
   layout 'mailer'
 end
